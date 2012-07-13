f3961d8e98f441de45de7e844c87cb7f3263a68b
Martin Burchell <martinburchell@spamcop.net>
2012-07-13 10:53:10 +0100

    Try to address problems with UnicodeDecodeError when running from cron or piping output

--- check_requests.py
+++ check_requests.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
 import os.path
+import codecs
+import locale
 import sys
 
 from connect_settings import *
 
 from connect_website import ConnectWebsite
+
+sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
 script_dir = os.path.dirname(sys.argv[0])
 connect_dir = os.path.join(script_dir, 'connect')
